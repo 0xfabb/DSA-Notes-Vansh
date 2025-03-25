520bdbae468eb3da2fafa90fb9d02f7cd57f0f9b
0xfabb <vanshdev2305@gmail.com>
2025-03-25 19:22:43 +0530

    Added Day 1 Notes and Programs.

---
+++ Day 1/first.cpp
@@ -0,0 +1,18 @@
+// 1. Simple program to print sum of 2 numbers a and b 
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+
+    cout << "Enter the  value of a \n";
+    cin >> a;
+    cout << "Enter the value of b \n";
+    cin >> b;
+    int sum = a + b;
+    cout << "The sum of a and b is " << sum << endl;
+
+    return 0;
+}
